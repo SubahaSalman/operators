e6ee58634e1e2c15f9b32e1a10953a9099f75b5f
SubahaSalman <subaha.salman@gmail.com>
2025-07-06 20:57:02 +0200

    ascii value

---
+++ ascii value.py
@@ -0,0 +1,55 @@
+character  = input("Enter a letter of the alphabet, capital: ")
+
+
+if character is "A":
+    print:(65)
+elif character is "B":
+    print(66)
+elif character is "C":
+    print(67)
+elif character is "D":
+    print(68)
+elif character is "E":
+    print(69)
+elif character is "F":
+    print(70)
+elif character is "G":
+    print(71)
+elif character is "H":
+    print(72)
+elif character is "I":
+    print(73)
+elif character is "J":
+    print(74)
+elif character is "K":
+    print(75)
+elif character is "L":
+    print(76)
+elif character is "M":
+    print(77)
+elif character is "N":
+    print(78)
+elif character is "O":
+    print(79)
+elif character is "P":
+    print(80)
+elif character is "Q":
+    print(81)
+elif character is "R":
+    print(82)
+elif character is "S":
+    print(83)
+elif character is "T":
+    print(84)
+elif character is "U":
+    print(85)
+elif character is "V":
+    print(86)
+elif character is "W":
+    print(87)
+elif character is "X":
+    print(88)
+elif character is "Y":
+    print(89)
+elif character is "Z":
+    print(90)
